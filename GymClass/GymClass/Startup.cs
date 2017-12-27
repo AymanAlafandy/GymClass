ddf7f3daee909ccce97929f02fe7b34efc4098ee
ayman alafandy <a.alafandy@outlook.sa>
2017-12-27 16:28:11 +0100

    Gymclass001

---
+++ GymClass/GymClass/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GymClass.Startup))]
+namespace GymClass
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
