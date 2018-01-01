bee1b584a1a2bc5fd8e04f9196fcebabfca7b8a7
ayman alafandy <myprogram@barid.com>
2018-01-01 13:10:54 +0100

    I add a comment to be sure that i will start from a branch.

--- GymClass/GymClass/Controllers/HomeController.cs
+++ GymClass/GymClass/Controllers/HomeController.cs
@@ -6,13 +6,16 @@ using System.Web.Mvc;
 
 namespace GymClass.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : Controller 
     {
         public ActionResult Index()
         {
             return View();
         }
-
+        /// <summary>
+        /// this is comment
+        /// </summary>
+        /// <returns></returns>
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
